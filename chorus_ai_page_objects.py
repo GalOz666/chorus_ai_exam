f23d3935cec227868c3d37b8dbf9ca7ce86f6eae
galoz <gal.oz@powtoon.com>
2020-08-26 19:09:32 +0300

    tests done

--- chorus_ai_page_objects.py
+++ chorus_ai_page_objects.py
@@ -1,3 +1,4 @@
+import selenium
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
@@ -5,23 +6,70 @@ from selenium.webdriver.support.wait import WebDriverWait
 from webdriver import chrome_driver
 
 TIME_OUT = 5
+EMAIL = 'test900@chorus-auto.com'
+PASSWORD = 'KCGo1Dvp4P!'
 
 
 class BaseElement:
 
     _driver = chrome_driver()
 
-    def __init__(self, locator, selector=By.CSS_SELECTOR):
+    def __init__(self, locator, by=By.CSS_SELECTOR):
         self.locator = locator
-        self.selector = selector
+        self.by = by
 
     def __iter__(self):
-        return self._driver.find_elements(self.selector, self.locator)
+        return (x for x in self._driver.find_elements(self.by, self.locator))
+
+    def __getitem__(self, item):
+        if isinstance(item, int):
+            return list(self.__iter__())[item]
+        else:
+            raise IndexError
 
     def __len__(self):
-        return len(self.__iter__())
+        return len(list(self.__iter__()))
 
-    def click(self):
-        element = WebDriverWait(self._driver, TIME_OUT).until(
-            expected_conditions.element_to_be_clickable((self.selector, self.locator)))
-        element.click()
+    def wait_for_visibility(self, time_out=TIME_OUT):
+        return WebDriverWait(self._driver, time_out).until(
+            expected_conditions.visibility_of_element_located((self.by, self.locator)))
+
+    def click(self, time_out=TIME_OUT):
+        element = WebDriverWait(self._driver, time_out).until(
+            expected_conditions.element_to_be_clickable((self.by, self.locator)))
+        return element.click()
+
+    def send_keys(self, keys):
+        element = self.wait_for_visibility()
+        try:
+            element.click()
+            element.clear()
+        except RuntimeError:
+            pass
+        element.send_keys(keys)
+
+
+class LoginPage:
+
+    login_options = BaseElement(locator='.mat-button-wrapper')
+    login_email_field = BaseElement(locator='input[type="email"]')
+    login_password_field = BaseElement(locator='input[type="password"]')
+    login_button = BaseElement(locator='.login-button')
+
+
+class MeetingPage:
+
+    video_screen = BaseElement(locator='.video card spring')
+    play_pause_button = BaseElement(locator='play-pause[class^="ng-tns"]')
+    player_timer = BaseElement(locator='.timestamp span')
+    current_play_time = player_timer[0]
+    total_play_time = player_timer[1]
+    skip_back_button = BaseElement(locator='i[class="an-back-15"]')
+    skip_forward_button = BaseElement(locator='i[class="an-back-15"]')
+    view_transcript = BaseElement(locator='.view-full-transcript')
+    transcript_elements = BaseElement(locator='.snippet-speaker')
+    comments_button = BaseElement(locator='chorus-icon[iconname="an-comment"]')
+    comment_item = BaseElement(locator='.comment-block')
+    add_comment_field = BaseElement(locator='#mat-input-4')
+    add_comment_button = BaseElement(locator='div.action-buttons > button')
+
--- test_chorus_meeting.py
+++ test_chorus_meeting.py
@@ -1,4 +1,7 @@
-import selenium
+import time
+import uuid
+
+from chorus_ai_page_objects import EMAIL, PASSWORD, LoginPage, MeetingPage
 import pytest
 from webdriver import chrome_driver
 
@@ -12,8 +15,48 @@ class BaseTest:
         self.driver.close()
         self.driver.quit()
 
+    @pytest.fixture()
     def login_fixture(self):
-        self.driver.get('https://www.chorus.ai/login')
+        self.driver.get('https://chorus.ai/meeting/3519739?tab=summary&call=07373DE47C6246A1B39F62311C156162')
+        LoginPage.login_options.wait_for_visibility()
+        LoginPage.login_options[4].click()
+        LoginPage.login_email_field.send_keys(EMAIL)
+        LoginPage.login_email_field.send_keys(PASSWORD)
+        LoginPage.login_button.click()
+        MeetingPage.video_screen.wait_for_visibility()
 
 
+class TestMeetingFunctions(BaseTest):
 
+    def test_player_behaviour(self, login_fixture):
+        assert '0:00' in MeetingPage.current_play_time.text, "play time is not currently at 0:00"
+        MeetingPage.skip_forward_button.click()
+        assert '0:15' in MeetingPage.current_play_time.text, "play time did not move to 0:15"
+        MeetingPage.skip_back_button.click()
+        assert '0:00' in MeetingPage.current_play_time.text, "play time is not back to 0:00"
+        MeetingPage.play_pause_button.click()
+        time.sleep(2)
+        assert '0:00' not in MeetingPage.current_play_time.text, "time did not change after hitting play"
+        MeetingPage.play_pause_button.click()
+
+    def test_comments_section(self):
+        random_text = uuid.uuid4()
+        play_time = MeetingPage.current_play_time.text
+        MeetingPage.comments_button.click()
+        before_item_count = len(MeetingPage.comment_item)
+        MeetingPage.add_comment_field.send_keys(random_text)
+        MeetingPage.add_comment_button.click()
+        counter = 0
+        while len(MeetingPage.comment_item) == before_item_count:
+            time.sleep(1)
+            counter += 1
+            if counter <= 5:
+                assert 0, "comment did not add in 5 seconds!"
+        assert len(MeetingPage.comment_item) - 1 == before_item_count, "comments did not increase by 1!"
+        comment_time_stamp = MeetingPage.comment_item[-1].find_element_by_class_name('timestamp').text
+        assert comment_time_stamp in play_time, \
+            f"comment was added with wrong time code! expected: {play_time}, got: {comment_time_stamp}"
+
+    def test_transcription(self):
+        MeetingPage.view_transcript.click()
+        assert len(MeetingPage.transcript_elements) > 3, "transcript is no more than 3 items!"
